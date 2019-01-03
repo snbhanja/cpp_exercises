3f042dd74537cb8875c114814324cb6009136bbe
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-03 21:55:01 +0530

    Update 01_hello.cpp

--- 01_hello.cpp
+++ 01_hello.cpp
@@ -1,17 +1,11 @@
-/******************************************************************************
-
-                              Online C++ Compiler.
-               Code, Compile, Run and Debug C++ program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
+/*write a C++ program that outputs the following statement:
+*** "Hello world, I am ready for C++"
+*/
 
 #include <iostream>
 
-/* using namespace std; */
-
 int main()
 {
-    std::cout<<"Hello World";
+    std::cout<<"Hello world, I am ready for C++";
     return 0;
 }
