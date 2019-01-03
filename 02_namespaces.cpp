0bf1b9e044bd4fc2f67ca52b3953b477dc6c724f
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-03 22:06:45 +0530

    Create 02_namespaces.cpp

---
+++ 02_namespaces.cpp
@@ -0,0 +1,15 @@
+/* how to compile and run.
+g++ main.cpp -o main.out
+./main.out
+*/
+
+/*Use the namespace keyword to simplify typing*/
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hey, writing std:: is pain, ";
+    std::cout << "change the program so I don't have to write it.";
+    return 0;
+}
