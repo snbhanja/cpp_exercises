b680ceb5d8fb8954b027b2ecea8ee446ff827da8
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-04 18:08:12 +0530

    Update 04_variable_type_size.cpp

--- 04_variable_type_size.cpp
+++ 04_variable_type_size.cpp
@@ -1,21 +1,20 @@
 /*GOAL: Practice writing to the console and learn 
-**the variables types available in C++
-**Print the sizes of each variable to the console.
-**Print them in the following order:
-**int, short, long, char, float, double, bool
-**
-**Use the command sizeof(variable type) ie: sizeof(int)
-*/
-#include <iostream>
+ **the variables types available in C++
+ **Print the sizes of each variable to the console.
+ **Print them in the following order:
+ **int, short, long, char, float, double, bool
+ */
 
-int main()
-{
-    std::cout << "Size of int =  " << sizeof(int) << std::endl;
-    std::cout << "Size of short =  " << sizeof(short) << std::endl;
-    std::cout << "Size of long =  " << sizeof(long) << std::endl;
-    std::cout << "Size of char =  " << sizeof(char) << std::endl;
-    std::cout << "Size of float =  " << sizeof(float) << std::endl;
-    std::cout << "Size of double =  " << sizeof(double) << std::endl;
-    std::cout << "Size of bool =  " << sizeof(bool) << std::endl;
-}
-
+ #include <iostream>
+ int main()
+ {
+     using namespace std;
+     cout<<"int size = "<<sizeof(int)<<"\n";
+     cout<<"short size = "<<sizeof(short)<<"\n";
+     cout<<"long size = "<<sizeof(long)<<"\n";
+     cout<<"char size = "<<sizeof(char)<<"\n";
+     cout<<"float size = "<<sizeof(float)<<"\n";
+     cout<<"double size = "<<sizeof(double)<<"\n";
+     cout<<"bool size = "<<sizeof(bool)<<"\n";
+     return 0;
+ }
