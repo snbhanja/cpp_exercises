796fde0f1067488697a829c2328fcf2aaad5ae98
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-05 23:43:29 +0530

    Create 21_relational_operators.cpp

---
+++ 21_relational_operators.cpp
@@ -0,0 +1,31 @@
+/*Goal: learn if-else statements in C++*/
+
+#include<iostream>
+#include<string>
+
+int main()
+{
+    //instead of printing 0 and 1, create an array where 
+    //0 = False, 1 = True
+    std::string TorF[] = {"False", "True"};
+    
+    int a = 100;
+    int b = 33;
+    int c = 33;
+    
+    //Print out the string values of each relational operation
+    std::cout<<"a < b is "<<TorF[a<b];
+    std::cout<<"\na > b is "<<TorF[a>b];
+    std::cout<<"\na != b is "<<TorF[a!=b];
+    std::cout<<"\nc >= b is "<<TorF[c>=b];
+    std::cout<<"\nc <= b is "<<TorF[c<=b];    
+    return 0;
+}
+
+/*
+a < b is False
+a > b is True
+a != b is True
+c >= b is True
+c <= b is True
+*/
