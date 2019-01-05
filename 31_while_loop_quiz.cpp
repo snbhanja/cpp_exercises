60bf6fa0792cab8970c4992cd202b11b0e1f2a7d
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-06 01:45:39 +0530

    Create 31_while_loop_quiz.cpp

---
+++ 31_while_loop_quiz.cpp
@@ -0,0 +1,51 @@
+/*Goal: In the programming quiz, use a while loop to prompt
+**the user to guess a target number.
+**Tell the user if the guess is too high or too low.
+**The user enters -1  or guesses the target number to end
+**the program.
+*/
+#include <iostream>
+#include<sstream>
+
+int main()
+{
+    int target = 55;
+    std::string userString;
+    int guess = -1;
+
+    while(guess != target )
+    {
+        std::cout<<"Guess a number between 0 and 100: ";
+        std::cin>>guess;
+        std::cout<<guess<<"\n";
+        if(guess > target)
+            std::cout<<"Your guess is too high\n";
+        else if(guess < target)
+            std::cout<<"Your guess is too low\n";
+        else
+            std::cout<<"You guessed the target!\n";
+
+        //Note I had to use double quotes around "q"
+        //because it is a string
+        if(guess == -1)
+        {
+            std::cout<<"good bye!";
+            break;
+        }
+    }
+    return 0;
+}
+
+
+/*
+Guess a number between 0 and 100: 6
+6
+Your guess is too low
+Guess a number between 0 and 100: 78
+78
+Your guess is too high
+Guess a number between 0 and 100: -1
+-1
+Your guess is too low
+good bye!
+*/
