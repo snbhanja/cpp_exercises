d98235c66674013059a8985c8379b0c425987e17
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-06 02:03:01 +0530

    Create 33_address_of_a_variable.cpp

---
+++ 33_address_of_a_variable.cpp
@@ -0,0 +1,16 @@
+/*Goal: Examine pointers!*/
+
+#include <iostream>
+
+int main()
+{
+    int a = 54;
+    std::cout<<"a = "<<a<<"\n";
+    std::cout<<"address of a is at &a = "<< &a<<"\n";
+    return 0;
+}
+
+/*
+a = 54
+address of a is at &a = 0x7fff09de849c
+*/
