910f5584c0c42ca78ad02f1bf1451c7b69e26ab7
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-05 10:46:58 +0530

    Create 10_header_files.cpp

---
+++ 10_header_files.cpp
@@ -0,0 +1,23 @@
+/*Goal: practice using header files.
+**. 
+**Put in the header file every thing that is related to 
+**"how to do a task".
+**Put the "what to do" in the main program.
+*/
+
+/* The main.hpp header file as below,
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+*/
+
+#include "main.hpp"
+
+int main()
+{
+    cout<<"Hello, I use header files!";
+    return 0;
+}
+
