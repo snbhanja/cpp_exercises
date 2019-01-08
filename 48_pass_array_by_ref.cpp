f723b92f048e69515330dc69e754690806c74ea2
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-08 10:43:51 +0530

    Create 48_pass_array_by_ref.cpp

---
+++ 48_pass_array_by_ref.cpp
@@ -0,0 +1,63 @@
+/*Goal: Learn to pass arrays to functions*/
+
+/*
+There are three methods for passing an array by reference to a function:
+
+void functionName(variableType *arrayName)
+void functionName(variableType arrayName[length of array])
+void functionName(variableType arrayName[])
+*/
+
+#include<iostream>
+#include<iomanip>
+
+//Pass the array as a pointer
+void arrayAsPointer(int *array, int size);
+//Pass the array as a sized array
+void arraySized(int array[3], int size);
+//Pass the array as an unsized array
+void arrayUnSized(int array[], int size);
+
+int main()
+{
+    const int size = 3;
+    int array[size] = {33,66,99};
+    //We are passing a pointer or reference to the array
+    //so we will not know the size of the array
+    //We have to pass the size to the function as well
+    arrayAsPointer(array, size);
+    arraySized(array, size);
+    arrayUnSized(array, size);
+    return 0;
+}
+
+void arrayAsPointer(int *array, int size)
+{
+    std::cout<<std::setw(5);
+    for(int i=0; i<size; i++) 
+        std::cout<<array[i]<<" ";
+    std::cout<<"\n";
+}
+
+void arraySized(int array[3], int size)
+{
+    std::cout<<std::setw(5);
+    for(int i=0; i<size; i++)
+        std::cout<<array[i]<<" ";
+    std::cout<<"\n";  
+}
+
+void arrayUnSized(int array[], int size)
+{
+    std::cout<<std::setw(5);
+    for(int i=0; i<size; i++)
+        std::cout<<array[i]<<" ";
+    std::cout<<"\n";  
+}
+
+
+/*
+33 66 99 
+33 66 99 
+33 66 99 
+*/
