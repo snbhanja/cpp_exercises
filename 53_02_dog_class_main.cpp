a785be50cad02d533906ee62275e1faa1498c882
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-10 00:36:14 +0530

    Create 53_02_dog_class_main.cpp

---
+++ 53_02_dog_class_main.cpp
@@ -0,0 +1,26 @@
+/*Goal: practice creating and using a class*/
+/*Goal: practice creating and using a class
+**Use the class 'Dog' to create to 
+**instances of the class, dog1 and dog2.
+**Assign dog1 the name: Trixie
+**Assign dog2 the name: Kali
+**Assign dog1 the license #1234
+**Assign dog2 the license #5678
+*/
+
+
+#include"53_01_dog_class_header.hpp"
+
+int main()
+{
+    Dog dog1, dog2;
+    dog1.setName("Trixie");
+    dog2.setName("Kali");
+    dog1.setLicenseNumber(1234);
+    dog2.setLicenseNumber(5678);
+
+    dog1.printInfo();
+    cout<<"\n";
+    dog2.printInfo();
+    return 0;
+}
