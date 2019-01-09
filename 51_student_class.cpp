a83f58bf8cdb7bd682a8daa3eeb49392dcd84bd2
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-10 00:24:06 +0530

    Create 51_student_class.cpp

---
+++ 51_student_class.cpp
@@ -0,0 +1,54 @@
+class Student
+{
+        string name;
+        int id;
+        int gradDate;
+
+    public:
+        void setName(string nameIn);
+        void setId(int idIn);
+        void setGradDate(int dateIn);
+        string getName();
+        int getId();
+        int getGradDate();
+        void print();
+};
+
+void Student::setName(string nameIn)
+{
+     name = nameIn;
+}
+
+void Student::setId(int idIn)
+{
+     id = idIn;
+}
+
+void Student::setGradDate(int gradDateIn)
+{
+     gradDate = gradDateIn;
+}
+
+void Student::print()
+{
+     cout<<name<<" "<<id<<" "<<gradDate;
+}
+
+string Student::getName()
+{
+     return name;
+}
+
+int Student::getId()
+{
+     return id; 
+}
+
+int Student::getGradDate()
+{
+     return gradDate;
+}
+
+/*
+
+*/
