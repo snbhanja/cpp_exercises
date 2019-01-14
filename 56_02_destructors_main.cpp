5720aa7494cae16dda764e0e5f0df139f68433c1
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-14 20:32:50 +0530

    Create 56_02_destructors_main.cpp

---
+++ 56_02_destructors_main.cpp
@@ -0,0 +1,12 @@
+/*Goal: use pointers to allot memory locations
+*for a class. 
+*/
+
+#include "56_01_destructors_main.hpp"
+
+int main()
+{
+    Dog d2(666666);
+    cout<<d2.getLicense();
+    return 0;
+}
