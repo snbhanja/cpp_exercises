1936332448a32bc15fb47927385c5d5b855ef499
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-15 20:33:06 +0530

    Update 57_02_class_helper_func.cpp

--- 57_02_class_helper_func.cpp
+++ 57_02_class_helper_func.cpp
@@ -1,11 +1,13 @@
 /*Goal: look at helper functions in classes*/
 /*
-Classes can have member functions that do more than just set or get data values. These helper functions can perform tasks that are often requested or are easier to perform in the class itself. 
+Classes can have member functions that do more than just set or get data values. These helper functions can perform tasks 
+that are often requested or are easier to perform in the class itself. 
 
 For example:
-Let's say we have a class that tracks the grades a student receives in a course. We can create a member function that calculates the average.
+Let's say we have a class that tracks the grades a student receives in a course. We can create a member function that 
+calculates the average.
 */
-#include "main.hpp"
+#include "57_01_class_helper_func.hpp"
 
 int main()
 {
