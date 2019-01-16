e97139cf4d788c845c0b9c3235a30d0be4654d79
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-16 22:08:55 +0530

    Create 65_02_constructor_parameter.cpp

---
+++ 65_02_constructor_parameter.cpp
@@ -0,0 +1,16 @@
+/*Goal: examine constructors with parameters*/
+
+#include "main.hpp"
+
+int main()
+{
+    //an instance of Patient is
+    //instanciated with a name
+    Patient p1("Tammy Smith");  
+    cout<<p1.getName();
+    return 0;
+}
+
+/*
+Tammy Smith
+*/
