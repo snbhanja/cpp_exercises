e1607c4470eb1c8dd263e338e2fa5b7d2a39c536
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-16 21:51:18 +0530

    Update 61_03_TicTacToeMain.cpp

--- 61_03_TicTacToeMain.cpp
+++ 61_03_TicTacToeMain.cpp
@@ -3,6 +3,24 @@
 **play a 4x4 tic-tac-toe game. 
 */
 
+/*
+For this programming quiz, I would like you to create a game called TicTacToe. In this version of the game you will need to:
+
+* Create a 4x4 game board
+* Prompt the first user (the 'x' user) to enter their name 
+  Prompt the second user (the 'o' user) to enter their name
+* Prompt the 'x' user to select a grid position where they would like to place an 'x'. 
+  Prompt the 'o' user to select a grid position where they would like to place an 'o'.
+* After each user has a turn, check for any row, column, diagonal that has 4 'x's or 4 'o's.
+* If 4 'x's are found in the same col, row, diagonal, declare the 'x' user the winner. 
+  If 4 'o's are found in the same col, row, diagonal, declare the 'o' user the winner.
+* End the game and declare the winner.
+* If the grid is filled (each player gets 8 turns) and there is not a row, column, diagonal 
+with 4 of the same symbol, the game is tied. Declare a tie game.
+main.cpp
+
+*/
+
 #include "TicTacToeClasses.cpp"
 #include "TicTacToeFunctions.cpp"
 
