aa0f71d95e323a75a6b2116a5da619e4d3f72c26
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-16 22:46:25 +0530

    Create 67_02_overloading_constructor.cpp

---
+++ 67_02_overloading_constructor.cpp
@@ -0,0 +1,16 @@
+/*Goal: learn about overloading constructors*/
+
+#include "main.hpp"
+
+int main()
+{
+    Square s1;
+    Square s2(4,3);
+    cout<<"s1 dimensions are: "<<s1.getWidth()<<","<<s1.getLength();
+    cout<<"\ns2 dimensions are: "<<s2.getWidth()<<","<<s2.getLength();
+    return 0;
+}
+
+/*
+
+*/
