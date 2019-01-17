e3ed499f596576d664aee045922c8c7c648d0204
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-17 22:40:33 +0530

    Create 75_02_multiple_inheritance_main.cpp

---
+++ 75_02_multiple_inheritance_main.cpp
@@ -0,0 +1,15 @@
+
+
+/*Goal: understand multiple inheritance*/
+
+#include "75_01_multiple_inheritance_header.hpp"
+
+int main()
+{
+    TA t1;
+    t1.setSupervisor("Dr. Caohuu");
+    t1.setId(55555);
+    t1.setTitle("Adjunct Prof.");
+    cout<<t1.getSupervisor()<<" "<<t1.getId()<<" "<<t1.getTitle();
+    return 0;
+}
