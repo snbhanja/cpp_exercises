71998f5002d8288a6d877a8e4a116407b6b52b5c
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-17 22:33:10 +0530

    Create 74_01_inheritance_main.cpp

---
+++ 74_01_inheritance_main.cpp
@@ -0,0 +1,23 @@
+
+
+/*Goal: study a simple example if inheritance. 
+*/
+#include"74_01_inheritance_header.hpp"
+
+int main()
+{
+    //Create an instance of the derived class, 
+    //GradStudent
+    GradStudent gs1;
+    
+    //Set the id of the grad student
+    //using the function in the base class
+    gs1.setId(123456789);
+    
+    //Set the degree of the grad student
+    //using the function in the derived class
+    gs1.setDegree("BSEE");
+    
+    cout<<gs1.getId()<<" "<<gs1.getDegree();
+    return 0;
+}
