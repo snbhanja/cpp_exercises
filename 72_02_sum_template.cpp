bbc1f88a260cf62de3c5bce307f3ccbde2ff582b
Satyanarayan Bhanja <snbhanja@users.noreply.github.com>
2019-01-17 21:52:44 +0530

    Create 72_02_sum_template.cpp

---
+++ 72_02_sum_template.cpp
@@ -0,0 +1,16 @@
+/*Goal: Practice writing generic functions*/
+
+#include "72_02_sum_template.hpp"
+
+int main()
+{
+    int a = 90;
+    int b = 1; 
+    std::cout<<a <<" + "<<b<<" = "<< sumTwo(a,b);
+    return 0;
+}
+
+
+/*
+90 + 1 = 91
+*/
